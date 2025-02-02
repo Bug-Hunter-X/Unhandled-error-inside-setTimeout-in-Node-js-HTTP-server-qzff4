412cc88e1cf102fc4fc75c30290080ec7946b39f
BugHunter-X <askzenai@gmail.com>
2025-02-02 07:34:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Simulate an asynchronous operation that might throw an error
+  setTimeout(() => {
+    if (req.url === '/error') {
+      throw new Error('Something went wrong!');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello World!');
+    }
+  }, 1000);
+});
+
+server.on('error', (err) => {
+  console.error('Server Error:', err);
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
